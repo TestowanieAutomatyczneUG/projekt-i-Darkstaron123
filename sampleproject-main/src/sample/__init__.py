ad63e5e44dd541e9bca71cbb8d35398fa1d343d2
Mateusz Mackowiak <darkstaron123@gmail.com>
2021-12-21 16:08:42 +0100

    I have copied sample project structure

---
+++ sampleproject-main/src/sample/__init__.py
@@ -0,0 +1,26 @@
+def main():
+    """Entry point for the application script"""
+    #print("Call your main application code here")
+    import json
+
+    data = {}
+    data['people'] = []
+    data['people'].append({
+        'name': 'Scott',
+        'website': 'stackabuse.com',
+        'from': 'Nebraska'
+    })
+    data['people'].append({
+        'name': 'Larry',
+        'website': 'google.com',
+        'from': 'Michigan'
+    })
+    data['people'].append({
+        'name': 'Tim',
+        'website': 'apple.com',
+        'from': 'Alabama'
+    })
+    print(data)
+    with open('../../data/data.txt', 'w') as outfile:
+        json.dump(data, outfile)
+main()
