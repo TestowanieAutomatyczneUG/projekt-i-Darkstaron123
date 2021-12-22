de09bb34758c63e72b8e127482bff0f1dc04e762
Mateusz Mackowiak <darkstaron123@gmail.com>
2021-12-22 23:24:38 +0100

    Dodalem import eksport do pliku .csv

--- ProjectElectronicDictionaryMateuszMackowiak/src/sample/menuClass.py
+++ ProjectElectronicDictionaryMateuszMackowiak/src/sample/menuClass.py
@@ -1,6 +1,7 @@
 class MenuClass:
     def menu(language):
         from discipleClass import DiscipleClass
+        from menuClass import MenuClass
         if (language == 'PL'):
             print('Czesc, zalogowales sie do dziennika. Wybierz opcje, poprzez wpisanie jej numeru:')
             print('0. Wybierz i wyswietl ucznia.')
@@ -20,13 +21,18 @@ class MenuClass:
             elif (choose == "4"):
                 print("Wylogowales sie!")
                 return 0
+            else:
+                print('Miales literowke. Sproboj jeszcze raz!')
+                return menu(language)
         elif (language == 'EN'):
             print('Hello, you logged in electronic dictionary. Choose option, by typing in it\'s number:')
             print('0. Choose and show disciple')
             print('1. Add disciple')
             print('2. Edit disciple')
             print('3. Delete disciple')
-            print('4. Log Out')
+            print('4. Import database from csv file')
+            print('5. Export database to csv file')
+            print('6. Log Out')
             choose = str(input())
             if (choose == "0"):
                 DiscipleClass.chooseAndDisplayDisciple(language)
@@ -37,12 +43,15 @@ class MenuClass:
             elif (choose == "3"):
                 DiscipleClass.removeDisciple(language)
             elif (choose == "4"):
+                MenuClass.importDatabaseFromCSV(language)
+            elif (choose == "5"):
+                MenuClass.exportDatabaseToCSV(language)
+            elif (choose == "6"):
                 print("You have logged out!")
                 return 0
-        else:
-            print('You had a typo. Try again!')
-            print('Miales literowke. Sproboj jeszcze raz!')
-            return menu(language)
+            else:
+                print('You had a typo. Try again!')
+                return menu(language)
 
     def chooseLanguage():
         print('To choose english language, type in: EN')
@@ -50,3 +59,19 @@ class MenuClass:
         choose = str(input())
         language = choose
         return language
+    def exportDatabaseToCSV(language):
+        from menuClass import MenuClass
+        import json
+        with open('../../data/data.txt') as json_file:
+            data = json.load(json_file)
+        with open('../../data/data.csv', 'w') as outfile:
+            json.dump(data, outfile)
+        return MenuClass.menu(language)
+    def importDatabaseFromCSV(language):
+        from menuClass import MenuClass
+        import json
+        with open('../../data/data.csv') as json_file:
+            data = json.load(json_file)
+        with open('../../data/data.txt', 'w') as outfile:
+            json.dump(data, outfile)
+        return MenuClass.menu(language)
