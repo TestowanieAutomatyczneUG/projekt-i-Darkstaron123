a1898a20459d6a682ca85cc4ca2eed4d87a8deb6
Mateusz Mackowiak <darkstaron123@gmail.com>
2021-12-22 17:35:47 +0100

    Dodanie dodawania ocen i usuwania ich

--- ProjectElectronicDictionaryMateuszMackowiak/src/sample/addMark.py
+++ ProjectElectronicDictionaryMateuszMackowiak/src/sample/addMark.py
@@ -1,2 +1,25 @@
 def addMark(language,discipleId,subjectId):
-    pass
+    import json
+    from editSubject import editSubject
+    if (language == "EN"):
+        print("You entered process of adding mark. Type in mark you want to add.")
+        with open('../../data/data.txt') as json_file:
+            data = json.load(json_file)
+            print("Marks:")
+            print(data['disciples'][int(discipleId)]['subjects'][int(subjectId)]['marks'])
+            choose = str(input())
+        with open('../../data/data.txt', 'w') as outfile:
+            data['disciples'][int(discipleId)]['subjects'][int(subjectId)]['marks'].append(str(choose))
+            json.dump(data, outfile)
+        return editSubject(language, discipleId)
+    if (language == "PL"):
+        print("Weszles w proces usuwania ucznia. Wybierz ucznia poprzez wpisanie jego Id z listy ponizej,")
+        with open('../../data/data.txt') as json_file:
+            data = json.load(json_file)
+            print("Marks:")
+            print(data['disciples'][int(discipleId)]['subjects'][int(subjectId)]['marks'])
+            choose = str(input())
+        with open('../../data/data.txt', 'w') as outfile:
+            data['disciples'][int(discipleId)]['subjects'][int(subjectId)]['marks'].append(str(choose))
+            json.dump(data, outfile)
+        return editSubject(language, discipleId)
