7cec1c2c60f4970aee864e4b2a1284e0f7d3e1a5
Mateusz Mackowiak <darkstaron123@gmail.com>
2021-12-23 17:48:38 +0100

    test_calculateMarkAverageFromDisciple_result_is_not_none

--- ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
+++ ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
@@ -186,6 +186,11 @@ class DiscipleClassTest(unittest.TestCase):
         with open('../data/test_data.txt') as json_file:
             data = json.load(json_file)
             self.assertFalse(type(self.temp.calculateMarkAverageFromDisciple(data['disciples'][0]))==memoryview)
+    def test_calculateMarkAverageFromDisciple_result_is_not_none(self):
+        import json
+        with open('../data/test_data.txt') as json_file:
+            data = json.load(json_file)
+            self.assertIsNotNone(type(self.temp.calculateMarkAverageFromDisciple(data['disciples'][0])))
 
 # mark average =============================================================================================
 
