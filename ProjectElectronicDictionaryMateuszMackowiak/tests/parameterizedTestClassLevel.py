30aa5d7aabdcfb781372ce6926a73ae43b5637d7
Mateusz Mackowiak <darkstaron123@gmail.com>
2021-12-23 23:05:59 +0100

    Uzylem nostestow w konfiguracji i pytesta

--- ProjectElectronicDictionaryMateuszMackowiak/tests/parameterizedTestClassLevel.py
+++ ProjectElectronicDictionaryMateuszMackowiak/tests/parameterizedTestClassLevel.py
@@ -11,5 +11,5 @@ class MenuClass_chooseLanguage_ClassLevel(unittest.TestCase):#Poziom klasy
     def setUp(self):
         self.tmp = MenuClass()
 
-    def test_class_parameterized(self):
+    def test_choose_language_parameterized(self):
         self.assertEqual(self.tmp.chooseLanguage(self.language), self.expected)
