622eef6bcd45ba6c0c4cbfda9adb07aac156feab
Mateusz Mackowiak <darkstaron123@gmail.com>
2021-12-23 19:43:42 +0100

    test_countNumberOfNotices_is_not_complex

--- ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
+++ ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
@@ -195,6 +195,11 @@ class DiscipleClassTest(unittest.TestCase):
         with open('../data/test_data.txt') as json_file:
             data = json.load(json_file)
             self.assertNotEqual(type(self.temp.countNumberOfNotices(data['disciples'][0])),float)
+    def test_countNumberOfNotices_is_not_complex(self):
+        import json
+        with open('../data/test_data.txt') as json_file:
+            data = json.load(json_file)
+            self.assertNotEqual(type(self.temp.countNumberOfNotices(data['disciples'][0])),complex)
 # notices quantity from disciple========================================================================================
 # mark average from disciple============================================================================================
     def test_calculateMarkAverageFromDisciple_is_result_float(self):
