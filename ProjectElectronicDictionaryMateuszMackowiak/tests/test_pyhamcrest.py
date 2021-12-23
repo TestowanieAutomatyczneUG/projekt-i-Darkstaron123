308e8f05b18332e1b1d6e1b2bc06cb8f79572b78
Mateusz Mackowiak <darkstaron123@gmail.com>
2021-12-23 20:17:31 +0100

    test_calculateMarkAverageFromDisciple_is_not_dict

--- ProjectElectronicDictionaryMateuszMackowiak/tests/test_pyhamcrest.py
+++ ProjectElectronicDictionaryMateuszMackowiak/tests/test_pyhamcrest.py
@@ -52,3 +52,9 @@ class PyHamcrestAverageCalculatorTest(unittest.TestCase):
         with open('../data/test_data.txt') as json_file:
             data = json.load(json_file)
             assert_that(type(self.temp.calculateMarkAverageFromDisciple(data['disciples'][0])),is_not(range))
+    def test_calculateMarkAverageFromDisciple_is_not_dict(self):
+        import json
+        with open('../data/test_data.txt') as json_file:
+            data = json.load(json_file)
+            assert_that(type(self.temp.calculateMarkAverageFromDisciple(data['disciples'][0])),is_not(dict))
+    
