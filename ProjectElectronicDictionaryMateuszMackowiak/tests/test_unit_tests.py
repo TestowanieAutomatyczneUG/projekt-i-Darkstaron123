bc6734b791a8723de6d6822afa28c8575fee082a
Mateusz Mackowiak <darkstaron123@gmail.com>
2021-12-23 19:47:25 +0100

    test_countNumberOfNotices_is_not_bool

--- ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
+++ ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
@@ -230,6 +230,11 @@ class DiscipleClassTest(unittest.TestCase):
         with open('../data/test_data.txt') as json_file:
             data = json.load(json_file)
             self.assertNotEqual(type(self.temp.countNumberOfNotices(data['disciples'][0])),frozenset)
+    def test_countNumberOfNotices_is_not_bool(self):
+        import json
+        with open('../data/test_data.txt') as json_file:
+            data = json.load(json_file)
+            self.assertNotEqual(type(self.temp.countNumberOfNotices(data['disciples'][0])),bool)
 # notices quantity from disciple========================================================================================
 # mark average from disciple============================================================================================
     def test_calculateMarkAverageFromDisciple_is_result_float(self):
