91075e7946c968fac59ce43296f7939a0410b226
Mateusz Mackowiak <darkstaron123@gmail.com>
2021-12-23 18:28:34 +0100

    test_calculateMarkAverageFromSubject_result_is_not_complex

--- ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
+++ ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
@@ -224,6 +224,11 @@ class DiscipleClassTest(unittest.TestCase):
         with open('../data/test_data.txt') as json_file:
             data = json.load(json_file)
             self.assertIsNot(type(self.temp.calculateMarkAverageFromDisciple(data['disciples'][0])),int)
+    def test_calculateMarkAverageFromSubject_result_is_not_complex(self):
+        import json
+        with open('../data/test_data.txt') as json_file:
+            data = json.load(json_file)
+            self.assertIsNot(type(self.temp.calculateMarkAverageFromDisciple(data['disciples'][0])),complex)
 
     # mark average from subject=============================================================================================
     @patch('discipleClass.DiscipleClass.displayAllDisciples')
