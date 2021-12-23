d9995f8f6ceb51d7f8c1050d9d5693cb69b617d0
Mateusz Mackowiak <darkstaron123@gmail.com>
2021-12-23 18:27:39 +0100

    test_calculateMarkAverageFromSubject_result_is_not_int

--- ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
+++ ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
@@ -219,6 +219,11 @@ class DiscipleClassTest(unittest.TestCase):
         with open('../data/test_data.txt') as json_file:
             data = json.load(json_file)
             self.assertIsNot(type(self.temp.calculateMarkAverageFromDisciple(data['disciples'][0])),str)
+    def test_calculateMarkAverageFromSubject_result_is_not_int(self):
+        import json
+        with open('../data/test_data.txt') as json_file:
+            data = json.load(json_file)
+            self.assertIsNot(type(self.temp.calculateMarkAverageFromDisciple(data['disciples'][0])),int)
 
     # mark average from subject=============================================================================================
     @patch('discipleClass.DiscipleClass.displayAllDisciples')
