237bdbd4dcd6f6b051fc980a8dd25f7a0c49e827
Mateusz Mackowiak <darkstaron123@gmail.com>
2021-12-23 17:45:20 +0100

    test_calculateMarkAverageFromDisciple_result_is_not_bytes

--- ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
+++ ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
@@ -171,6 +171,11 @@ class DiscipleClassTest(unittest.TestCase):
         with open('../data/test_data.txt') as json_file:
             data = json.load(json_file)
             self.assertFalse(type(self.temp.calculateMarkAverageFromDisciple(data['disciples'][0]))==bool)
+    def test_calculateMarkAverageFromDisciple_result_is_not_bool(self):
+        import json
+        with open('../data/test_data.txt') as json_file:
+            data = json.load(json_file)
+            self.assertFalse(type(self.temp.calculateMarkAverageFromDisciple(data['disciples'][0]))==bytes)
 
 # mark average =============================================================================================
 
