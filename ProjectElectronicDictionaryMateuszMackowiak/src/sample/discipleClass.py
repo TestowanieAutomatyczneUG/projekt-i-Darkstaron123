375844e8809d2735742711685518cd066b806832
Mateusz Mackowiak <darkstaron123@gmail.com>
2021-12-23 16:22:23 +0100

    Poprawilem usuwanie ucznia dla wyjatkow

--- ProjectElectronicDictionaryMateuszMackowiak/src/sample/discipleClass.py
+++ ProjectElectronicDictionaryMateuszMackowiak/src/sample/discipleClass.py
@@ -285,38 +285,40 @@ class DiscipleClass:
             print("You entered process of removing disciple. Choose disciple by typing in his Id from list below.")
             DiscipleClass.displayAllDisciples(language)
             choose = str(input())
-            with open('../../data/data.txt') as json_file:
-                data = json.load(json_file)
-            with open('../../data/data.txt', 'w') as outfile:
-                try:
-                    data['disciples'][int(choose)]
-                except:
-                    print("No disciple with provided id.")
+            try:
+                with open('../../data/data.txt') as json_file:
+                    data = json.load(json_file)
+                if (len(data['disciples']) > int(choose) and int(choose) >= 0):
+                    with open('../../data/data.txt', 'w') as outfile:
+                        del data['disciples'][int(choose)]
+                        number = 0  # reassigning id after deletion
+                        for i in data['disciples']:
+                            i['id'] = str(number)
+                            number = number + 1
+                        json.dump(data, outfile)
+                else:
                     return MenuClass.menu(language)
-                del data['disciples'][int(choose)]
-                number = 0  # reassigning id after deletion
-                for i in data['disciples']:
-                    i['id'] = str(number)
-                    number = number + 1
-                json.dump(data, outfile)
+            except:
+                return MenuClass.menu(language)
             return MenuClass.menu(language)
         if (language == "PL"):
             print("Weszles w proces usuwania ucznia. Wybierz ucznia poprzez wpisanie jego Id z listy ponizej,")
             DiscipleClass.displayAllDisciples(language)
             choose = str(input())
-            with open('../../data/data.txt') as json_file:
-                data = json.load(json_file)
-            with open('../../data/data.txt', 'w') as outfile:
-                try:
-                    data['disciples'][int(choose)]
-                except:
-                    print("Brak ucznia o podanym id.")
+            try:
+                with open('../../data/data.txt') as json_file:
+                    data = json.load(json_file)
+                if (len(data['disciples']) > int(choose) and int(choose) >= 0):
+                    with open('../../data/data.txt', 'w') as outfile:
+                        del data['disciples'][int(choose)]
+                        number = 0  # reassigning id after deletion
+                        for i in data['disciples']:
+                            i['id'] = str(number)
+                            number = number + 1
+                        json.dump(data, outfile)
+                else:
                     return MenuClass.menu(language)
-                del data['disciples'][int(choose)]
-                number = 0  # reassigning id after deletion
-                for i in data['disciples']:
-                    i['id'] = str(number)
-                    number = number + 1
-                json.dump(data, outfile)
+            except:
+                return MenuClass.menu(language)
             return MenuClass.menu(language)
 
