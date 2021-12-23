154b64243d2c31495605973c53f4867f7b2e6540
Mateusz Mackowiak <darkstaron123@gmail.com>
2021-12-23 20:19:18 +0100

    test_calculateMarkAverageFromDisciple_is_not_bytes

--- ProjectElectronicDictionaryMateuszMackowiak/tests/test_pyhamcrest.py
+++ ProjectElectronicDictionaryMateuszMackowiak/tests/test_pyhamcrest.py
@@ -72,3 +72,8 @@ class PyHamcrestAverageCalculatorTest(unittest.TestCase):
         with open('../data/test_data.txt') as json_file:
             data = json.load(json_file)
             assert_that(type(self.temp.calculateMarkAverageFromDisciple(data['disciples'][0])),is_not(bool))
+    def test_calculateMarkAverageFromDisciple_is_not_bytes(self):
+        import json
+        with open('../data/test_data.txt') as json_file:
+            data = json.load(json_file)
+            assert_that(type(self.temp.calculateMarkAverageFromDisciple(data['disciples'][0])),is_not(bytes))
