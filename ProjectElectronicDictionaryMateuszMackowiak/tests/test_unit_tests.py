360b110116f8035e1a85e9624b41f8138e5594fb
Mateusz Mackowiak <darkstaron123@gmail.com>
2021-12-23 18:44:24 +0100

    test_choose_language_is_not_set

--- ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
+++ ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
@@ -126,6 +126,8 @@ class MenuClassTest(unittest.TestCase):
         self.assertNotEqual(type(self.temp.chooseLanguage("EN")),range)
     def test_choose_language_is_not_dict(self):
         self.assertNotEqual(type(self.temp.chooseLanguage("EN")),dict)
+    def test_choose_language_is_not_set(self):
+        self.assertNotEqual(type(self.temp.chooseLanguage("EN")),set)
     def exception_choose_language_other(self):
         self.assertRaises(Exception("Wrong language inputed."),self.temp.chooseLanguage("Uga Booga"))
 class DiscipleClassTest(unittest.TestCase):
