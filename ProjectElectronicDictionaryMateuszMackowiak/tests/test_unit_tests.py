b01eb9a16a655d58e82576ff1c6a94730ffd7970
Mateusz Mackowiak <darkstaron123@gmail.com>
2021-12-23 17:41:55 +0100

    test_calculateMarkAverageFromDisciple_result_is_not_range

--- ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
+++ ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
@@ -146,6 +146,11 @@ class DiscipleClassTest(unittest.TestCase):
         with open('../data/test_data.txt') as json_file:
             data = json.load(json_file)
             self.assertFalse(type(self.temp.calculateMarkAverageFromDisciple(data['disciples'][0]))==tuple)
+    def test_calculateMarkAverageFromDisciple_result_is_not_range(self):
+        import json
+        with open('../data/test_data.txt') as json_file:
+            data = json.load(json_file)
+            self.assertFalse(type(self.temp.calculateMarkAverageFromDisciple(data['disciples'][0]))==range)
 
 # mark average =============================================================================================
 
