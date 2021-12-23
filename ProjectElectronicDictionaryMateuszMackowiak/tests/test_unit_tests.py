08c1a4d1f95f12ea31015cac5a24b670d66da99b
Mateusz Mackowiak <darkstaron123@gmail.com>
2021-12-23 19:45:25 +0100

    test_countNumberOfNotices_is_not_range

--- ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
+++ ProjectElectronicDictionaryMateuszMackowiak/tests/test_unit_tests.py
@@ -210,6 +210,11 @@ class DiscipleClassTest(unittest.TestCase):
         with open('../data/test_data.txt') as json_file:
             data = json.load(json_file)
             self.assertNotEqual(type(self.temp.countNumberOfNotices(data['disciples'][0])),tuple)
+    def test_countNumberOfNotices_is_not_range(self):
+        import json
+        with open('../data/test_data.txt') as json_file:
+            data = json.load(json_file)
+            self.assertNotEqual(type(self.temp.countNumberOfNotices(data['disciples'][0])),range)
 # notices quantity from disciple========================================================================================
 # mark average from disciple============================================================================================
     def test_calculateMarkAverageFromDisciple_is_result_float(self):
